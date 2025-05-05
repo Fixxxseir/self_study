e603c2c37fe4826385380d1a0622b4b4aac996d7
mexic <mexic2284@mail.ru>
2025-05-05 21:32:27 +0400

    к путям юзера добавлены пути для изменения пороля и профиля юзера

--- users/urls.py
+++ users/urls.py
@@ -2,28 +2,34 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (
-	TokenObtainPairView,
-	TokenRefreshView,
+    TokenObtainPairView,
+    TokenRefreshView,
 )
 
 from users.apps import UsersConfig
-from users.views import UserRegisterAPIView
+from users.views import CustomPasswordChange, UserMe, UserRegisterAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-	path("register/", UserRegisterAPIView.as_view(), name="user-register"),
-
-	path(
-		"login/",
-		TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
-		name="user-login",
-	),
-	path(
-		"token/refresh/",
-		TokenRefreshView.as_view(permission_classes=(AllowAny,)),
-		name="user-token_refresh",
-	),
-	path(
-		"logout/", LogoutView.as_view(next_page="catalog:home"),name="logout"),
+    path("register/", UserRegisterAPIView.as_view(), name="user-register"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="user-login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="user-token_refresh",
+    ),
+    path(
+        "settings/password/change",
+        CustomPasswordChange.as_view(),
+        name="users-password-change",
+    ),
+    path(
+        "logout/", LogoutView.as_view(next_page="catalog:home"), name="logout"
+    ),
+    path("profile/", UserMe.as_view(), name="user-profile"),
 ]
