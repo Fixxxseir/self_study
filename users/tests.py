9c29b3d253b697a71141d1dfbfe0bc3f186fff84
mexic <mexic2284@mail.ru>
2025-05-05 21:28:48 +0400

    добавлены тесты регистрация юзера, суперюзера

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class TestEmailUserManagerAddProf(APITestCase):
+    def test_create_superuser_is_incorrect(self):
+        """Создаем первого superuser"""
+        User.objects.create_superuser(
+            email="email@emial.ru",
+            username="superuser",
+            password="superuser",
+        )
+        user = User.objects.get(email="email@emial.ru")
+        self.assertEqual(user.email, "email@emial.ru")
+        self.assertEqual(user.role, "admin")
+
+        User.objects.create_superuser(
+            email="email2@emial.ru",
+            username="superuser",
+            password="superuser",
+        )
+        user = User.objects.get(email="email2@emial.ru")
+        self.assertEqual(user.email, "email2@emial.ru")
+
+        User.objects.create_user(
+            email="email3@emial.ru",
+            username="student",
+            password="student",
+        )
+        user = User.objects.get(email="email3@emial.ru")
+        self.assertEqual(user.email, "email3@emial.ru")
+        self.assertEqual(user.role, "student")
